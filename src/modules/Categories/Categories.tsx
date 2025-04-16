2cd534a53db3827ec1537b2b1210f762c88d93a3
AnnetaDe <anneta.liss@gmail.com>
2025-04-16 13:18:12 +0200

    categories

--- src/modules/Categories/Categories.tsx
+++ src/modules/Categories/Categories.tsx
@@ -5,6 +5,40 @@ import TextTitle from "@/shared/components/TextTitle/TextTitle";
 import Image from "next/image";
 
 const Categories = () => {
+
+    const ellipsePositions = [
+        '-top-5 -left-3',
+        '-bottom-5 -left-3',
+        '-bottom-4 -right-2',
+        '-bottom-4 -left-5',
+        '-bottom-8 -left-8',
+    ]
+
+    const ellipseFills = [
+        '#545454',
+        '#D88164',
+        '#D88164',
+        '#D88164',
+        '#545454',
+    ]
+
+
+    const textPositions = [
+        'top-2 left-4',
+        'bottom-2 right-4',
+        'top-1 left-1 ',
+        'bottom-3 right-3',
+        '-bottom-0 left-5',
+    ]
+
+    const subtitlePositions = [
+        'top-1/2 left-20',
+        'bottom-1/2 right-4',
+        'top-1/2 left-20 ',
+        'bottom-1/2 right-3',
+        'bottom-1/2 left-1/3',
+    ]
+
     return <div className="w-full items-center justify-center">
         <TextTitle text="КATEГОРІЇ ТОВАРІВ" className="text-center mb-8 font-medium" fontSize="20px" />
         <ul className="flex flex-col gap-4 ">
@@ -13,29 +47,34 @@ const Categories = () => {
 
                 <li
                     key={item.id}
-                    className={`flex h-[100px] w-full max-w-4xl overflow-hidden rounded-lg border border-[var(--main-dark-color)] ${index % 2 === 0
+                    className={`relative flex h-[100px] w-full max-w-4xl overflow-hidden rounded-lg border border-[var(--main-dark-color)] ${index % 2 === 0
                         ? 'bg-[var(--main-dark-color)] text-white flex-row-reverse'
-                        : 'bg-amber-100 text-black flex-row'
-                        }`}
+                        : 'bg-white text-black flex-row'
+                        }`
+
+
+                    }
                 >
                     <EllipseBackground
-                        className="absolute pointer-events-none"
-                        fill={index % 2 === 0 ? '#545454' : '#D88164'}
-                        width={180}
-                        height={100}
+                        className={`absolute opacity-90 pointer-events-none ${ellipsePositions[index] || ''}`}
+                        fill={ellipseFills[index] || '#D88164'}
+                        width={188}
+                        height={101}
+
                     />
+
+
+
                     <div
-                        className="w-[120px] h-[100px] bg-no-repeat bg-bottom bg-auto border-2 border-red-600"
+                        className="relative w-[120px] h-[100px] bg-no-repeat bg-bottom bg-auto shrink-0 "
                         style={{ backgroundImage: `url(${item.img})` }}
                     >
-
-
                     </div>
 
 
-                    <div className="overflow-hidden flex flex-col justify-center">
-                        <TextTitle text={item.text} className="flex text-center mb-2 font-medium" fontSize="14px" />
-                        <TextSubtitle text={item.signature} className="flex text-center" fontSize="12px" color="[var(--main-dark-color)] " />
+                    <div className={` flex flex-col justify-center z-30 `}>
+                        <TextTitle text={item.text} className={`absolute flex text-center mb-2 font-medium uppercase  ${textPositions[index] || ''}`} fontSize="14px" />
+                        <TextSubtitle text={item.signature} className={`absolute flex text-center ${subtitlePositions[index] || ''}`} fontSize="12px" />
                     </div>
                 </li>
             ))}
