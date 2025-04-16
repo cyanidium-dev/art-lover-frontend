d9f78c422a1fc3bff523471662e23e4e56695a0a
AnnetaDe <anneta.liss@gmail.com>
2025-04-16 13:45:12 +0200

    buttons

--- src/modules/Categories/Categories.tsx
+++ src/modules/Categories/Categories.tsx
@@ -7,8 +7,6 @@ import CategoryItem from "./CategoryItem/CategoryItem";
 
 const Categories = () => {
 
-
-
     return <div className="w-full items-center justify-center">
         <TextTitle text="КATEГОРІЇ ТОВАРІВ" className="text-center mb-8 font-medium" fontSize="20px" />
         <ul className="flex flex-col gap-4 ">
