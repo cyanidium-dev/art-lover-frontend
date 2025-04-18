c9b9af38da1e0863c437e14f9b831e8d023cee04
AnnetaDe <anneta.liss@gmail.com>
2025-04-19 00:41:07 +0200

    Update Reviews.tsx

--- src/modules/Reviews/Reviews.tsx
+++ src/modules/Reviews/Reviews.tsx
@@ -15,34 +15,8 @@ const Reviews = (props: Props) => {
 
           <ul className="flex gap-4 overflow-auto mb-8" >
               {reviewsItems.map((item) => (
-                  <ReviewItem item={item} key={item.id} />
-                //   <li key={item.id} className="w-[200px] h-[358px] flex flex-col gap-3 items-left rounded-md bg-white shadow-lg p-[12px] border-1 border-transparent">
-                //       <div className="w-[176px] h-[85px] rounded-md">
-                //       <Image src={item.img} alt={item.name} className="object-cover w-full"
-                //               width={176} height={85} />
-                //       </div>
-                //       <div className="">
-                //           <p className="text-[12px] font-normal text-left">Товар:</p>
-                //           <p className="text-[14px] font-medium text-left">{item.product}</p>
-                //       </div>
-                //       <div className="max-h-[145px] overflow-auto">
-                //           <p className="font-normal text-[12px] text-left  leading-tight">"{item.comment}"</p>
+                <ReviewItem item={item} key={item.id} />
 
-                //       </div>
-
-                //       <div className="flex gap-1 items-center">
-                //           {Array.from({ length: item.rating }, (_, index) => (
-                //               <span key={index} ><Star height={20} width={20} fill="var(--main-orange)" stroke="var(--main-orange)" /></span>
-                //           ))}
-                //       </div>
-                      
-                //       <div className="flex gap-2 ">
-                //           <TextSubtitle text={item.name} className="text-left font-normal" fontSize="20px" />
-                //           <p className="font-normal text-[12px] text-left">{item.date }</p>
-                         
-                //       </div>
-                       
-                //   </li>
                 ))}
              
           </ul>
