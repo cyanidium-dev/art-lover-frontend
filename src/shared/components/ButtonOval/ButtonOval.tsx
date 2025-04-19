884a6e318524998c8c32aa17b349516d12b0e6a6
AnnetaDe <anneta.liss@gmail.com>
2025-04-19 17:10:36 +0200

    Update ButtonOval.tsx

--- src/shared/components/ButtonOval/ButtonOval.tsx
+++ src/shared/components/ButtonOval/ButtonOval.tsx
@@ -13,7 +13,7 @@ type Props = {
 const ButtonOval = (props: Props) => {
     return (
         <button
-            className={`btn flex ${props.className} ${props.variant} ${props.size} cursor-pointer rounded-2xl text-[var(--main-dark-color)]  px-2 py-2 `}
+            className={`btn flex ${props.className} ${props.variant} ${props.size} cursor-pointer rounded-2xl text-[var(--main-dark-color)]  px-2 py-2 items-center justify-center`}
             onClick={props.onClick}
             style={{
                 background: 'linear-gradient(121deg, #FFF 22.48%, #FF692D 337.32%)',
@@ -21,7 +21,7 @@ const ButtonOval = (props: Props) => {
                 border: '1px solid transparent',
                 ...props.style,
             }}
-        >   {props.svg && <span className="mr-1">{props.svg}</span>}
+        >   
             <span className="truncate">{props.buttonText}</span>
 
 
