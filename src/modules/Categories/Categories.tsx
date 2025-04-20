0594e4224c2d79f819937fc4ea339ad6dfff4764
AnnetaDe <anneta.liss@gmail.com>
2025-04-20 21:54:51 +0200

    upd

--- src/modules/Categories/Categories.tsx
+++ src/modules/Categories/Categories.tsx
@@ -7,17 +7,18 @@ import SliderControllers from "@/shared/components/SliderControllers/SliderContr
 
 const Categories = () => {
 
-    return <div className="w-full items-center justify-center py-8 ">
-        <TextTitle text="КATEГОРІЇ ТОВАРІВ" className="text-center mb-6 font-medium" fontSize="20px" />
-        <ul className="flex flex-col gap-4 mb-8 ">
-            {categoryItems.map((item, index) => (
-                <CategoryItem item={item} index={index} key={item.id} />
-
-
-            ))}
-        </ul>
-        <SliderControllers />
-    </div >
+    return (
+        <div className="w-full items-center justify-center py-8">
+            <TextTitle text="КATEГОРІЇ ТОВАРІВ" className="text-center mb-6 font-medium" fontSize="20px" />
+            <ul className="flex flex-col gap-2 mb-8 max-w-[1280px] mx-auto sm:grid sm:grid-cols-2 sm:gap-4 xl:flex xl:flex-col">
+                {categoryItems.map((item, index) => (
+                    <CategoryItem item={item} index={index} key={item.id} />
+                ))}
+            </ul>
+            <SliderControllers />
+        </div>
+    );
 }
 
 export default Categories;
+
--- src/modules/Gifts/Gifts.tsx
+++ src/modules/Gifts/Gifts.tsx
@@ -14,7 +14,7 @@ const Gifts = () => {
         <div className='py-8'>
             <div className='relative flex flex-col justify-end bg-[var(--main-orange)] rounded-2xl   mx-auto overflow-hidden shadow-[0px_4px_20px_rgba(0,0,0,0.25)]'>
 
-            <div className="absolute top-0 w-full h-[48px] bg-[var(--main-orange-light)]  bg-repeat-wrap bg-blend-lighten rounded-b-full"
+                <div className="absolute top-0 w-full h-[48px] bg-[var(--main-orange-light)]  bg-repeat-wrap bg-blend-lighten rounded-b-full sm:h-1/6 "
                 style={{
                     backgroundImage: "url('/logo.svg')",
                     backgroundSize: '110px 24px',
@@ -23,7 +23,7 @@ const Gifts = () => {
 
             </div>
             <div className='z-10 mt-12 px-4'>
-                <h2 className="text-white text-[14px] font-bold uppercase mb-1">
+                    <h2 className="text-white text-[14px] font-bold uppercase mb-1 sm:text-[50px] sm:mb-2  sm:font-black ">
                     Допоможемо обрати подарунок
                 </h2>
                 <p className="text-justify text-[12px] text-white font-light mb-[12px] leading-none">Наш сайт допоможе обрати подарунок для будь-якого свята та події за допомогою фільтрів</p>
@@ -42,10 +42,8 @@ const Gifts = () => {
             <Image
                 src={gifts}
                 alt="gift"
-                priority
-                width={0}
-                height={0}
-                sizes="100vw"
+
+
                 className="w-full h-auto object-bottom"
             />
             </div>
