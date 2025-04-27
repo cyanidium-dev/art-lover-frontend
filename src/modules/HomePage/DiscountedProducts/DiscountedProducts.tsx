4c0f7e9a45c64d523e776886c2f7ef14559ddc75
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-27 12:03:20 +0300

    updated product card

--- src/modules/HomePage/DiscountedProducts/DiscountedProducts.tsx
+++ src/modules/HomePage/DiscountedProducts/DiscountedProducts.tsx
@@ -1,6 +1,19 @@
+'use client';
+import { SwiperSlide } from 'swiper/react';
+import { productsList } from '../bestsellers/mockedData';
+import ProductCard from '@/shared/components/cards/ProductCard/ProductCard';
 import Container from '@/shared/components/container/Container';
+import SwiperWrapper from '@/shared/components/swiper/SwiperWrapper';
 
 export default function DiscountedProducts() {
+  if (!productsList || !productsList.length) return null;
+
+  const discountedProductsList = productsList.filter(
+    product => product.discountedPrice
+  );
+
+  if (!discountedProductsList || !discountedProductsList.length) return null;
+
   return (
     <section className="pt-20 xl:pt-[158px]">
       <Container>
@@ -13,6 +26,29 @@ export default function DiscountedProducts() {
           </p>
         </div>
       </Container>
+      <div className="md:max-w-[774px] lg:max-w-[1030px] xl:max-w-[1286px] md:px-8 xl:px-[90px] md:mx-auto">
+        <SwiperWrapper
+          swiperClassName="newProducts"
+          breakpoints={{
+            0: {
+              spaceBetween: 16,
+              centeredSlides: true,
+            },
+            768: { spaceBetween: 16, centeredSlides: false },
+            1024: { spaceBetween: 20, centeredSlides: false },
+            1280: {
+              spaceBetween: 20,
+              centeredSlides: false,
+            },
+          }}
+        >
+          {discountedProductsList.map((discountedProduct, idx) => (
+            <SwiperSlide key={idx}>
+              <ProductCard product={discountedProduct} />
+            </SwiperSlide>
+          ))}
+        </SwiperWrapper>
+      </div>
     </section>
   );
 }
--- src/shared/components/Cards/ProductCard/ProductCard.tsx
+++ src/shared/components/Cards/ProductCard/ProductCard.tsx
@@ -9,10 +9,19 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
-  const { title, price, images, slug } = product;
+  const { title, price, discountedPrice, images, slug } = product;
 
   return (
-    <div className="p-3 bg-white rounded-[8px] xl:rounded-[16px] shadow-social">
+    <div className="relative p-3 xl:px-5 xl:py-4 bg-white rounded-[8px] xl:rounded-[16px] shadow-social overflow-hidden">
+      {discountedPrice && discountedPrice < price ? (
+        <div
+          className="flex items-center gap-x-[11px] absolute z-10 top-0 md:top-[-12px] left-[19px] rotate-[29deg] w-[233px] xl:w-[278px] py-[5px] xl:py-[7px] pl-[44px] xl:pl-[25px] bg-orange 
+        text-white text-[8px] xl:text-[12px] font-semibold leading-[120%] uppercase"
+        >
+          <span>Акція</span> <span>Акція</span> <span>Акція</span>
+          <span>Акція</span> <span>Акція</span>
+        </div>
+      ) : null}
       <div className="relative h-[120px] md:h-[180px] lg:h-[200px] xl:h-[300px] mb-3 xl:mb-4 rounded-[8px] xl:rounded-[16px] overflow-hidden">
         <Image
           src={images[0]?.url || ''}
@@ -21,9 +30,22 @@ export default function ProductCard({ product }: ProductCardProps) {
           className="object-cover"
         />
       </div>
-      <p className="mb-1.5 xl:mb-2 text-[14px] xl:text-[16px] font-medium leading-[120%]">
-        {price} грн
-      </p>
+      {discountedPrice && discountedPrice < price ? (
+        <p className="mb-1.5 xl:mb-2 ">
+          <span className="text-[14px] xl:text-[16px] font-medium leading-[120%] text-orange">
+            {discountedPrice} грн
+          </span>
+          &nbsp;
+          <span className="text-[12px] xl:text-[14px] font-normal leading-[120%] line-through">
+            {price} грн
+          </span>
+        </p>
+      ) : (
+        <p className="mb-1.5 xl:mb-2 text-[14px] xl:text-[16px] font-medium leading-[120%]">
+          {price} грн
+        </p>
+      )}
+
       <h3 className="mb-4 xl:mb-[18px] text-[14px] xl:text-[18px] font-medium leading-[120%] line-clamp-1">
         {title}
       </h3>
