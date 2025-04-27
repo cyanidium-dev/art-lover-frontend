420f2c6060c19a34958775a7335208868aa68077
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-27 10:31:00 +0300

    added new products swiper

--- src/modules/HomePage/NewProducts/NewProducts.tsx
+++ src/modules/HomePage/NewProducts/NewProducts.tsx
@@ -1,6 +1,17 @@
+'use client';
+import { SwiperSlide } from 'swiper/react';
+import { productsList } from '../bestsellers/mockedData';
+import ProductCard from '@/shared/components/cards/ProductCard/ProductCard';
 import Container from '@/shared/components/container/Container';
+import SwiperWrapper from '@/shared/components/swiper/SwiperWrapper';
 
 export default function NewProducts() {
+  if (!productsList || !productsList.length) return null;
+
+  const newProductsList = productsList.filter(product => product.newProduct);
+
+  if (!newProductsList || !newProductsList.length) return null;
+
   return (
     <section className="pt-20 xl:pt-[158px]">
       <Container>
@@ -13,6 +24,29 @@ export default function NewProducts() {
           </p>
         </div>
       </Container>
+      <div className="md:max-w-[774px] lg:max-w-[1030px] xl:max-w-[1286px] md:px-8 xl:px-[90px] md:mx-auto">
+        <SwiperWrapper
+          swiperClassName="newProducts"
+          breakpoints={{
+            0: {
+              spaceBetween: 16,
+              centeredSlides: true,
+            },
+            768: { spaceBetween: 16, centeredSlides: false },
+            1024: { spaceBetween: 20, centeredSlides: false },
+            1280: {
+              spaceBetween: 20,
+              centeredSlides: false,
+            },
+          }}
+        >
+          {newProductsList.map((newProduct, idx) => (
+            <SwiperSlide key={idx}>
+              <ProductCard product={newProduct} />
+            </SwiperSlide>
+          ))}
+        </SwiperWrapper>
+      </div>
     </section>
   );
 }
