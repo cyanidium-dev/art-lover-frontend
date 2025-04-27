95de3425e3d0a8ce272dd5e8d1b99ce68677d43e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-27 10:39:18 +0300

    updated product card

--- src/shared/components/Cards/ProductCard/ProductCard.tsx
+++ src/shared/components/Cards/ProductCard/ProductCard.tsx
@@ -2,17 +2,18 @@ import { Product } from '@/types/product';
 import Image from 'next/image';
 import SecondaryButton from '../../buttons/SecondaryButton';
 import CartButton from './CartButton';
+import Link from 'next/link';
 
 interface ProductCardProps {
   product: Product;
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
-  const { title, price, images } = product;
+  const { title, price, images, slug } = product;
 
   return (
     <div className="p-3 bg-white rounded-[8px] xl:rounded-[16px] shadow-social">
-      <div className="relative h-[120px] md:h-[180px] lg:h-[200px] xl:h-[300px] mb-3 rounded-[8px] xl:rounded-[16px] overflow-hidden">
+      <div className="relative h-[120px] md:h-[180px] lg:h-[200px] xl:h-[300px] mb-3 xl:mb-4 rounded-[8px] xl:rounded-[16px] overflow-hidden">
         <Image
           src={images[0]?.url || ''}
           alt={images[0]?.url || 'product'}
@@ -20,14 +21,16 @@ export default function ProductCard({ product }: ProductCardProps) {
           className="object-cover"
         />
       </div>
-      <p className="mb-1.5 text-[14px] font-medium leading-[120%]">
+      <p className="mb-1.5 xl:mb-2 text-[14px] xl:text-[16px] font-medium leading-[120%]">
         {price} грн
       </p>
-      <h3 className="mb-4 text-[14px] font-medium leading-[120%] line-clamp-1">
+      <h3 className="mb-4 xl:mb-[18px] text-[14px] xl:text-[18px] font-medium leading-[120%] line-clamp-1">
         {title}
       </h3>
-      <div className="flex gap-x-4.5">
-        <SecondaryButton className="h-9">Детальніше</SecondaryButton>
+      <div className="flex gap-x-4.5 xl:gap-x-2">
+        <Link href={slug} className="block w-full">
+          <SecondaryButton className="h-9 xl:h-10">Детальніше</SecondaryButton>
+        </Link>
         <CartButton className="shrink-0" />
       </div>
     </div>
