8bb07ce8c44f618191f1983812634d2fcdf70f3e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-28 17:39:24 +0300

    added styles for mobile

--- src/modules/home/hero/HeroSlideOne.tsx
+++ src/modules/home/hero/HeroSlideOne.tsx
@@ -3,19 +3,29 @@ import Image from 'next/image';
 
 export default function HeroSlideOne() {
   return (
-    <div className="pb-9 bg-gray-light text-white">
-      <div className="relative h-[316px] mb-15 rounded-b-[24px] overflow-hidden">
-        <div className="absolute inset-0 z-10 hero-gradient"></div>
+    <div className="relative pb-9 bg-dark text-white overflow-hidden">
+      <div className="relative max-w-[480px] mx-auto">
         <Image
-          src="/images/homePage/hero/imageOne.webp"
-          alt="hero image"
+          src="/images/homePage/hero/logo.svg"
+          alt="logo"
           priority
-          fill
-          sizes="(max-width: 768px) 100vw, 50vw"
-          className="object-cover object-[center_94%]"
+          width="171"
+          height="145"
+          className="absolute z-30 bottom-[-44px] left-[calc(50%-86px)]"
         />
+        <div className="relative h-[316px] mb-15 rounded-b-[24px] overflow-hidden">
+          <div className="absolute inset-0 z-20 hero-gradient"></div>
+          <Image
+            src="/images/homePage/hero/imageOne.webp"
+            alt="hero image"
+            priority
+            fill
+            sizes="(max-width: 768px) 100vw, 50vw"
+            className="object-cover z-10 object-[center_74%]"
+          />
+        </div>
       </div>
-      <Container>
+      <div className="relative max-w-[480px] mx-auto px-8">
         <p className="max-w-[160px] mb-3 font-denistina text-[16px] font-normal leading-[120%]">
           Створи шедевр вже сьогодні!
         </p>
@@ -25,7 +35,23 @@ export default function HeroSlideOne() {
         <h1 className="max-w-[180px] mx-auto text-[16px] font-medium leading-[120%] uppercase text-center">
           Твоя творчість починається тут!
         </h1>
-      </Container>
+      </div>
+      <Image
+        src="/images/homePage/hero/figuresRightMob.svg"
+        priority
+        alt="background figures"
+        width="170"
+        height="298"
+        className="absolute bottom-[-22px] right-[-76px]"
+      />
+      <Image
+        src="/images/homePage/hero/figureLeftMob.svg"
+        priority
+        alt="background figure"
+        width="164"
+        height="104"
+        className="absolute bottom-[-31px] left-[-97px]"
+      />
     </div>
   );
 }
