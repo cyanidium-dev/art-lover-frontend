8c93e8979c49a9c988efa026ac2638d72b20c703
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-28 18:37:46 +0300

    added styles for desktop

--- src/modules/home/hero/Hero.tsx
+++ src/modules/home/hero/Hero.tsx
@@ -2,7 +2,7 @@ import HeroSlideOne from './HeroSlideOne';
 
 export default function Hero() {
   return (
-    <section className="h-[560px] xl:h-[734px]">
+    <section className="">
       <HeroSlideOne />
     </section>
   );
