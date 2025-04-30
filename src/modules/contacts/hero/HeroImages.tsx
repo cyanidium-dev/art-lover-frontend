8ffbf370820ac3ebf74ceab48a2fffb531834404
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-04-30 12:35:20 +0300

    added hero styles for desktop

--- src/modules/contacts/hero/HeroImages.tsx
+++ src/modules/contacts/hero/HeroImages.tsx
@@ -8,29 +8,29 @@ export default function HeroImages() {
         alt="background"
         width="140"
         height="154"
-        className="xl:hidden absolute -z-10 top-0 left-0"
+        className="md:hidden absolute -z-10 top-0 left-0"
       />
       <Image
         src="/images/contactsPage/hero/figureRightMob.svg"
         alt="background"
         width="275"
         height="122"
-        className="xl:hidden absolute -z-10 top-0 right-0"
+        className="md:hidden absolute -z-10 top-0 right-0"
       />
-      {/* <Image
+      <Image
         src="/images/contactsPage/hero/figureLeftDesk.svg"
         alt="background"
-        width="806"
-        height="1344"
-        className="hidden xl:block absolute -z-10 top-[-152px] left-[-239px]"
+        width="418"
+        height="277"
+        className="hidden md:block absolute -z-10 top-[14px] left-6 md:w-[300px] lg:w-[330px] xl:w-[418px] h-auto"
       />
       <Image
         src="/images/contactsPage/hero/figureRightDesk.svg"
         alt="background"
-        width="563"
-        height="2054"
-        className="hidden xl:block absolute -z-10 top-[-90px] right-[-139px]"
-      /> */}
+        width="671"
+        height="236"
+        className="hidden md:block absolute -z-10 top-0 right-0 md:w-[460px] lg:w-[520px] xl:w-[671px] h-auto"
+      />
     </>
   );
 }
