8fe9782807d46227278221927132f21722f2784c
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-01 21:08:55 +0300

    added help choose gift animation

--- src/modules/home/helpChooseGift/HelpChooseGift.tsx
+++ src/modules/home/helpChooseGift/HelpChooseGift.tsx
@@ -1,73 +1,161 @@
 import Container from '@/shared/components/container/Container';
 import Image from 'next/image';
 import Link from 'next/link';
+import * as motion from 'motion/react-client';
+import { fadeInAnimation } from '@/shared/utils/animationVariants';
 
 export default function HelpChooseGift() {
   return (
     <section className="pt-20 xl:pt-[94px]">
       <Container className="relative">
-        <Image
-          src="/images/homePage/helpChooseGifts/figureMob.svg"
-          alt="figure background"
-          width="102"
-          height="101"
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={fadeInAnimation({ scale: 0.9, delay: 1.6 })}
           className="xl:hidden absolute bottom-[-32px] left-1"
-        />
-        <div className="relative z-10 rounded-[16px] xl:rounded-[20px] bg-orange overflow-hidden">
+        >
           <Image
-            src="/images/homePage/helpChooseGifts/figureDesk.svg"
+            src="/images/homePage/helpChooseGifts/figureMob.svg"
             alt="figure background"
-            width="124"
-            height="237"
-            className="hidden xs:block absolute top-0 left-0 w-[74px] lg:w-[94px] xl:w-[124px] h-auto"
+            width="102"
+            height="101"
           />
-          <Image
-            src="/images/homePage/helpChooseGifts/logoBgMob.svg"
-            alt="logo background"
-            width="290"
-            height="51"
-            className="xs:hidden mb-6 w-full h-auto"
-          />
-          <Image
-            src="/images/homePage/helpChooseGifts/logoBgDesk.svg"
-            alt="logo background"
-            width="1100"
-            height="169"
-            className="hidden xs:block mb-11 w-full h-auto"
-          />
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={fadeInAnimation({ y: 30 })}
+          className="relative z-10 rounded-[16px] xl:rounded-[20px] bg-orange overflow-hidden"
+        >
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({ scale: 0.9, delay: 1.6 })}
+            className="hidden xs:block absolute top-0 left-0"
+          >
+            <Image
+              src="/images/homePage/helpChooseGifts/figureDesk.svg"
+              alt="figure background"
+              width="124"
+              height="237"
+              className="w-[74px] lg:w-[94px] xl:w-[124px] h-auto"
+            />
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({ y: -30, delay: 1.2, duration: 1.5 })}
+            className="xs:hidden mb-6 w-full"
+          >
+            <Image
+              src="/images/homePage/helpChooseGifts/logoBgMob.svg"
+              alt="logo background"
+              width="290"
+              height="51"
+              className="w-full h-auto"
+            />
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({ y: -30, delay: 1.6 })}
+            className="hidden xs:block mb-11 w-full"
+          >
+            <Image
+              src="/images/homePage/helpChooseGifts/logoBgDesk.svg"
+              alt="logo background"
+              width="1100"
+              height="169"
+              className="w-full h-auto"
+            />
+          </motion.div>
           <div className="relative z-20 pb-[232px] xs:pb-26 xl:pb-9 px-6 xl:px-14 ">
-            <h2 className="max-w-[286px] md:max-w-[306px] xl:max-w-[456px] mb-3 text-[18px] sm:text-[32px] xl:text-[50px] font-extrabold leading-[122%] uppercase text-white">
+            <motion.h2
+              initial="hidden"
+              whileInView="visible"
+              exit="exit"
+              viewport={{ once: true, amount: 0.2 }}
+              variants={fadeInAnimation({ y: 30, delay: 0.4 })}
+              className="max-w-[286px] md:max-w-[306px] xl:max-w-[456px] mb-3 text-[18px] sm:text-[32px] xl:text-[50px] font-extrabold leading-[122%] uppercase text-white"
+            >
               Допоможемо обрати подарунок
-            </h2>
-            <p className="max-w-[206px] md:max-w-[306px] xl:max-w-[396px] mb-5 xl:mb-[52px] text-[12px] xl:text-[16px] font-light leading-[125%] text-justify text-white">
+            </motion.h2>
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              exit="exit"
+              viewport={{ once: true, amount: 0.2 }}
+              variants={fadeInAnimation({ y: 30, delay: 0.8 })}
+              className="max-w-[206px] md:max-w-[306px] xl:max-w-[396px] mb-5 xl:mb-[52px] text-[12px] xl:text-[16px] font-light leading-[125%] text-justify text-white"
+            >
               Наш сайт допоможе обрати подарунок для будь-якого свята та події
               за допомогою фільтрів
-            </p>
+            </motion.p>
             <Link href="/">
-              <button
+              <motion.button
+                initial="hidden"
+                whileInView="visible"
+                exit="exit"
+                viewport={{ once: true, amount: 0.2 }}
+                variants={fadeInAnimation({ y: 30, delay: 1.2 })}
                 className="cursor-pointer flex items-center justify-center px-4 py-2 w-[116px] sm:w-full max-w-[236px] md:max-w-[306px] xl:max-w-[456px] h-8 sm:h-12 xl:h-[58px] rounded-full 
             text-[8px] sm:text-[12px] xl:text-[14px] font-normal leading-[120%] shadow-social bg-linear-[121deg,white_22.48%,#FF692D_337.32%] 
              sm:bg-linear-[-186deg,#ffffff_11.19%,#ff692d_199.95%] xl:hover:brightness-105 focus-visible:brightness-105 active:scale-95 transition duration-300 ease-in-out"
               >
                 Обрати подарунок
-              </button>
+              </motion.button>
             </Link>
           </div>
-          <Image
-            src="/images/homePage/helpChooseGifts/imageMob.webp"
-            alt="girl with gifts"
-            width="592"
-            height="550"
-            className="xs:hidden absolute z-10 bottom-0 right-0 w-full max-w-[380px] h-auto"
-          />
-          <Image
-            src="/images/homePage/helpChooseGifts/imageDesk.webp"
-            alt="girl with gifts"
-            width="2160"
-            height="1186"
-            className="hidden xs:block absolute z-10 bottom-0 right-0 w-full h-auto"
-          />
-        </div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({
+              y: 30,
+              delay: 1.6,
+              duration: 1.5,
+            })}
+            className="xs:hidden absolute z-10 bottom-0 right-0 w-full max-w-[380px]"
+          >
+            <Image
+              src="/images/homePage/helpChooseGifts/imageMob.webp"
+              alt="girl with gifts"
+              width="592"
+              height="550"
+              className="w-full max-w-[380px] h-auto"
+            />
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({
+              y: 30,
+              delay: 0.8,
+            })}
+            className="hidden xs:block absolute z-10 bottom-0 right-0 w-full"
+          >
+            <Image
+              src="/images/homePage/helpChooseGifts/imageDesk.webp"
+              alt="girl with gifts"
+              width="2160"
+              height="1186"
+              className="w-full h-auto"
+            />
+          </motion.div>
+        </motion.div>
       </Container>
     </section>
   );
