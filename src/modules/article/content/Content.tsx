9b9d425249c12c846d7683ff0b75eb5b0b16fb76
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-02 12:22:04 +0300

    added content animation

--- src/modules/article/content/Content.tsx
+++ src/modules/article/content/Content.tsx
@@ -1,4 +1,10 @@
 import Image from 'next/image';
+import * as motion from 'motion/react-client';
+import {
+  fadeInAnimation,
+  listVariants,
+  listItemVariants,
+} from '@/shared/utils/animationVariants';
 import { Article } from '@/types/article';
 import Container from '@/shared/components/container/Container';
 import ContentImages from './ContentImages';
@@ -13,21 +19,47 @@ export default function Content({ article }: ContentProps) {
     <section className="pt-8 xl:pt-12 pb-20 xl:pb-35">
       <Container className="relative">
         <ContentImages />
-        <h2 className="mb-4 xl:pb-6 text-[14px] xl:text-[16px] font-semibold leading-[120%]">
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={fadeInAnimation({ y: 30 })}
+          className="mb-4 xl:pb-6 text-[14px] xl:text-[16px] font-semibold leading-[120%]"
+        >
           {description}
-        </h2>
-        <ul className="flex flex-col gap-4 xl:gap-6 mb-8 xl:mb-12">
+        </motion.h2>
+        <motion.ul
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.4 }}
+          variants={listVariants({
+            staggerChildren: 0.4,
+            delayChildren: 0.8,
+          })}
+          className="flex flex-col gap-4 xl:gap-6 mb-8 xl:mb-12"
+        >
           {text.map((paragraph, idx) => (
-            <li
+            <motion.li
+              viewport={{ once: true, amount: 0.2 }}
+              variants={listItemVariants}
               key={idx}
               className="text-[12px] xl:text-[16px] font-normal leading-[160%] text-justify"
             >
               {paragraph}
-            </li>
+            </motion.li>
           ))}
-        </ul>
+        </motion.ul>
         <div className="flex flex-col md:flex-row gap-6">
-          <div className="relative w-full md:w-[calc(50%-12px)] h-[181px] md:h-[229px] xl:h-[329px] rounded-[16px] overflow-hidden shadow-social">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({ x: -30, delay: 0.4 })}
+            className="relative w-full md:w-[calc(50%-12px)] h-[181px] md:h-[229px] xl:h-[329px] rounded-[16px] overflow-hidden shadow-social"
+          >
             <Image
               src={images[0]?.url || ''}
               alt={images[0]?.alt || 'article image'}
@@ -35,8 +67,15 @@ export default function Content({ article }: ContentProps) {
               fill
               className="object-cover"
             />
-          </div>
-          <div className="relative md:w-[calc(50%-12px)] h-[181px] md:h-[229px] xl:h-[329px] rounded-[16px] overflow-hidden shadow-social">
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({ x: 30, delay: 0.4 })}
+            className="relative md:w-[calc(50%-12px)] h-[181px] md:h-[229px] xl:h-[329px] rounded-[16px] overflow-hidden shadow-social"
+          >
             <Image
               src={images[1]?.url || ''}
               alt={images[1]?.alt || 'article image'}
@@ -44,7 +83,7 @@ export default function Content({ article }: ContentProps) {
               fill
               className="object-cover"
             />
-          </div>
+          </motion.div>
         </div>
       </Container>
     </section>
--- src/modules/article/content/ContentImages.tsx
+++ src/modules/article/content/ContentImages.tsx
@@ -1,29 +1,55 @@
 import Image from 'next/image';
+import * as motion from 'motion/react-client';
+import { fadeInAnimation } from '@/shared/utils/animationVariants';
 
 export default function ContentImages() {
   return (
     <>
-      <Image
-        src="/images/articlePage/content/figureLeftTopMob.svg"
-        alt="background"
-        width="114"
-        height="92"
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({ delay: 1.2, scale: 0.9 })}
         className="xl:hidden absolute -z-10 top-[-32px] left-[-69px]"
-      />
-      <Image
-        src="/images/articlePage/content/figureRightMob.svg"
-        alt="background"
-        width="114"
-        height="92"
+      >
+        <Image
+          src="/images/articlePage/content/figureLeftTopMob.svg"
+          alt="background"
+          width="114"
+          height="92"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({ delay: 1.2, scale: 0.9 })}
         className="xl:hidden absolute -z-10 top-[423px] right-[-20px]"
-      />
-      <Image
-        src="/images/articlePage/content/figureLeftBottomMob.svg"
-        alt="background"
-        width="114"
-        height="92"
+      >
+        <Image
+          src="/images/articlePage/content/figureRightMob.svg"
+          alt="background"
+          width="114"
+          height="92"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({ delay: 1.2, scale: 0.9 })}
         className="xl:hidden absolute -z-10 top-[488px] left-[-54px]"
-      />
+      >
+        <Image
+          src="/images/articlePage/content/figureLeftBottomMob.svg"
+          alt="background"
+          width="114"
+          height="92"
+        />
+      </motion.div>
     </>
   );
 }
