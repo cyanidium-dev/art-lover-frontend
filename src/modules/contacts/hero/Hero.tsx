0dedcf70bcf121afc501c27d5298b082a245e489
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-02 10:55:17 +0300

    added hero animation

--- src/modules/contacts/hero/Hero.tsx
+++ src/modules/contacts/hero/Hero.tsx
@@ -1,3 +1,5 @@
+import * as motion from 'motion/react-client';
+import { fadeInAnimation } from '@/shared/utils/animationVariants';
 import Container from '@/shared/components/container/Container';
 import HeroImages from './HeroImages';
 
@@ -5,23 +7,58 @@ export default function Hero() {
   return (
     <section>
       <Container>
-        <div className="relative z-10 flex flex-col items-end justify-between py-2 md:pt-5 lg:pt-8 md:pb-5 lg:pb-7 px-3 md:px-20 lg:px-25 xl:px-[130.5px] bg-dark rounded-[8px] xl:rounded-[16px] text-white overflow-hidden">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={fadeInAnimation({})}
+          className="relative z-10 flex flex-col items-end justify-between py-2 md:pt-5 lg:pt-8 md:pb-5 lg:pb-7 px-3 md:px-20 lg:px-25 xl:px-[130.5px] bg-dark rounded-[8px] xl:rounded-[16px] text-white overflow-hidden"
+        >
           <HeroImages />
           <div className="max-w-[245px] md:max-w-[345px] xl:max-w-[435px] mb-8 md:mb-[43px]">
-            <h1 className="mb-2 md:mb-[13px] text-[20px] md:text-[32px] xl:text-[40px] font-medium leading-[120%] uppercase text-center">
+            <motion.h1
+              initial="hidden"
+              whileInView="visible"
+              exit="exit"
+              viewport={{ once: true, amount: 0.2 }}
+              variants={fadeInAnimation({ scale: 0.9, delay: 0.4 })}
+              className="mb-2 md:mb-[13px] text-[20px] md:text-[32px] xl:text-[40px] font-medium leading-[120%] uppercase text-center"
+            >
               контакти
-            </h1>
-            <h2 className="mb-2 md:mb-3 text-[14px] md:text-[16px] xl:text-[20px] font-semibold leading-[120%] xl:leading-[160%] text-center">
+            </motion.h1>
+            <motion.h2
+              initial="hidden"
+              whileInView="visible"
+              exit="exit"
+              viewport={{ once: true, amount: 0.2 }}
+              variants={fadeInAnimation({ y: 30, delay: 0.8 })}
+              className="mb-2 md:mb-3 text-[14px] md:text-[16px] xl:text-[20px] font-semibold leading-[120%] xl:leading-[160%] text-center"
+            >
               Маєте питання чи пропозиції?
-            </h2>
-            <p className="md:max-w-[240px] xl:max-w-[311px] xl:mx-auto text-[14px] xl:text-[18px] font-light leading-[120%] xl:leading-[160%] text-center">
+            </motion.h2>
+            <motion.p
+              initial="hidden"
+              whileInView="visible"
+              exit="exit"
+              viewport={{ once: true, amount: 0.2 }}
+              variants={fadeInAnimation({ y: 30, delay: 1.2 })}
+              className="md:max-w-[240px] xl:max-w-[311px] xl:mx-auto text-[14px] xl:text-[18px] font-light leading-[120%] xl:leading-[160%] text-center"
+            >
               Зв’яжіться з нами – ми з радістю допоможемо!
-            </p>
+            </motion.p>
           </div>
-          <p className="lg:mx-auto font-denistina text-[16px] md:text-[24px] xl:text-[32px] font-normal leading-[120%] xl:text-center">
+          <motion.p
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.2 }}
+            variants={fadeInAnimation({ x: -30, delay: 1.6 })}
+            className="lg:mx-auto font-denistina text-[16px] md:text-[24px] xl:text-[32px] font-normal leading-[120%] xl:text-center"
+          >
             Ваші&nbsp;&nbsp;питання - наші&nbsp;&nbsp;відповіді
-          </p>
-        </div>
+          </motion.p>
+        </motion.div>
       </Container>
     </section>
   );
--- src/modules/contacts/hero/HeroImages.tsx
+++ src/modules/contacts/hero/HeroImages.tsx
@@ -1,36 +1,72 @@
 import Image from 'next/image';
+import * as motion from 'motion/react-client';
+import { fadeInAnimation } from '@/shared/utils/animationVariants';
 
 export default function HeroImages() {
   return (
     <>
-      <Image
-        src="/images/contactsPage/hero/figureLeftMob.svg"
-        alt="background"
-        width="140"
-        height="154"
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({ delay: 0.8, duration: 2 })}
         className="md:hidden absolute -z-10 top-0 left-0"
-      />
-      <Image
-        src="/images/contactsPage/hero/figureRightMob.svg"
-        alt="background"
-        width="275"
-        height="122"
+      >
+        <Image
+          src="/images/contactsPage/hero/figureLeftMob.svg"
+          alt="background"
+          width="140"
+          height="154"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({})}
         className="md:hidden absolute -z-10 top-0 right-0"
-      />
-      <Image
-        src="/images/contactsPage/hero/figureLeftDesk.svg"
-        alt="background"
-        width="418"
-        height="277"
-        className="hidden md:block absolute -z-10 top-[14px] left-6 md:w-[300px] lg:w-[330px] xl:w-[418px] h-auto"
-      />
-      <Image
-        src="/images/contactsPage/hero/figureRightDesk.svg"
-        alt="background"
-        width="671"
-        height="236"
-        className="hidden md:block absolute -z-10 top-0 right-0 md:w-[460px] lg:w-[520px] xl:w-[671px] h-auto"
-      />
+      >
+        <Image
+          src="/images/contactsPage/hero/figureRightMob.svg"
+          alt="background"
+          width="275"
+          height="122"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({ delay: 0.8, duration: 2 })}
+        className="hidden md:block absolute -z-10 top-[14px] left-6"
+      >
+        <Image
+          src="/images/contactsPage/hero/figureLeftDesk.svg"
+          alt="background"
+          width="418"
+          height="277"
+          className=" md:w-[300px] lg:w-[330px] xl:w-[418px] h-auto"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({})}
+        className="hidden md:block absolute -z-10 top-0 right-0"
+      >
+        <Image
+          src="/images/contactsPage/hero/figureRightDesk.svg"
+          alt="background"
+          width="671"
+          height="236"
+          className=" md:w-[460px] lg:w-[520px] xl:w-[671px] h-auto"
+        />
+      </motion.div>
     </>
   );
 }
