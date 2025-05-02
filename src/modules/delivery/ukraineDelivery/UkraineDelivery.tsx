34a581dab58908d046532c82576ad361b9cad460
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-02 10:28:40 +0300

    added ukraine delivery animation

--- src/modules/delivery/ukraineDelivery/UkraineDelivery.tsx
+++ src/modules/delivery/ukraineDelivery/UkraineDelivery.tsx
@@ -1,3 +1,5 @@
+import * as motion from 'motion/react-client';
+import { fadeInAnimation } from '@/shared/utils/animationVariants';
 import Container from '@/shared/components/container/Container';
 import DeliveryList from './DeliveryList';
 import UkraineDeliveryImages from './UkraineDeliveryImages';
@@ -7,12 +9,26 @@ export default function UkraineDelivery() {
     <section className="pt-20 xl:pt-[218px]">
       <Container className="relative">
         <UkraineDeliveryImages />
-        <h2 className="mb-3 xl:mb-5 text-[20px] xl:text-[32px] font-medium leading-[120%] uppercase text-center">
+        <motion.h2
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={fadeInAnimation({ y: 30 })}
+          className="mb-3 xl:mb-5 text-[20px] xl:text-[32px] font-medium leading-[120%] uppercase text-center"
+        >
           Доставка по Україні
-        </h2>
-        <h3 className="mb-8 xl:mb-[30px] text-[14px] xl:text-[20px] font-medium leading-[120%] text-center">
+        </motion.h2>
+        <motion.h3
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.2 }}
+          variants={fadeInAnimation({ y: 30, delay: 0.4 })}
+          className="mb-8 xl:mb-[30px] text-[14px] xl:text-[20px] font-medium leading-[120%] text-center"
+        >
           Способи доставки:
-        </h3>
+        </motion.h3>
         <DeliveryList />
       </Container>
     </section>
--- src/modules/delivery/ukraineDelivery/UkraineDeliveryImages.tsx
+++ src/modules/delivery/ukraineDelivery/UkraineDeliveryImages.tsx
@@ -1,29 +1,55 @@
 import Image from 'next/image';
+import * as motion from 'motion/react-client';
+import { fadeInAnimation } from '@/shared/utils/animationVariants';
 
 export default function UkraineDeliveryImages() {
   return (
     <>
-      <Image
-        src="/images/deliveryPage/ukraineDelivery/figureLeftMob.svg"
-        alt="background"
-        width="114"
-        height="92"
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({ delay: 1.2, scale: 0.9 })}
         className="xl:hidden absolute -z-10 top-[195px] left-[-59px]"
-      />
-      <Image
-        src="/images/deliveryPage/ukraineDelivery/figureRightMob.svg"
-        alt="background"
-        width="114"
-        height="92"
+      >
+        <Image
+          src="/images/deliveryPage/ukraineDelivery/figureLeftMob.svg"
+          alt="background"
+          width="114"
+          height="92"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({ delay: 1.2, scale: 0.9 })}
         className="xl:hidden absolute -z-10 top-[-56px] right-[-20px]"
-      />
-      <Image
-        src="/images/deliveryPage/ukraineDelivery/figureRightDesk.svg"
-        alt="background"
-        width="401"
-        height="259"
+      >
+        <Image
+          src="/images/deliveryPage/ukraineDelivery/figureRightMob.svg"
+          alt="background"
+          width="114"
+          height="92"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        exit="exit"
+        viewport={{ once: true, amount: 0.2 }}
+        variants={fadeInAnimation({ delay: 1.2, scale: 0.9 })}
         className="hidden xl:block absolute -z-10 bottom-[-90px] right-[-120px]"
-      />
+      >
+        <Image
+          src="/images/deliveryPage/ukraineDelivery/figureRightDesk.svg"
+          alt="background"
+          width="401"
+          height="259"
+        />
+      </motion.div>
     </>
   );
 }
