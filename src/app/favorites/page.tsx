4c9b0467feeb52193542470fea69e121cae1ce50
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-02 16:26:18 +0300

    added hero with animations

---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,11 @@
+import FavoritesList from '@/modules/favorites/favoritesList/FavoritesList';
+import Hero from '@/modules/favorites/hero/Hero';
+
+export default function FavoritesPage() {
+  return (
+    <>
+      <Hero />
+      <FavoritesList />
+    </>
+  );
+}
---
+++ src/modules/favorites/favoritesList/FavoritesList.tsx
@@ -0,0 +1,3 @@
+export default function FavoritesList() {
+  return <section>FavoritesList</section>;
+}
