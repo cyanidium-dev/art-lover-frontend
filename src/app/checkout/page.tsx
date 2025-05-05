4ec0ac226217f575910173a8e65d57d5021eb3c0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-05 11:46:09 +0300

    added h1

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,9 @@
+import Checkout from '@/modules/checkout/Checkout';
+
+export default function CheckoutPage() {
+  return (
+    <>
+      <Checkout />
+    </>
+  );
+}
---
+++ src/modules/checkout/Checkout.tsx
@@ -0,0 +1,13 @@
+import Container from '@/shared/components/container/Container';
+
+export default function Checkout() {
+  return (
+    <section className="xl:pt-5 pb-20 xl:pb-[140px]">
+      <Container>
+        <h1 className="mb-5 xl:mb-7 text-[18px] xl:text-[32px] font-semibold leading-[140%] xl:leading-[120%] uppercase text-center">
+          Оформлення замовлення
+        </h1>
+      </Container>
+    </section>
+  );
+}
