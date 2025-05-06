030f61c127287ada13b8eee12ba2c92a6daf9c50
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-06 09:41:06 +0300

    added rating

--- src/modules/product/productInfo/OrderProduct.tsx
+++ src/modules/product/productInfo/OrderProduct.tsx
@@ -1,5 +1,12 @@
+'use client';
+import { Rating } from 'react-simple-star-rating';
+import IconButton from '@/shared/components/buttons/IconButton';
 import MainButton from '@/shared/components/buttons/MainButton';
+import StarFilledIcon from '@/shared/components/icons/StarFilledIcon';
+import StarEmptyIcon from '@/shared/components/icons/StarEmptyIcon';
 import { Product } from '@/types/product';
+import { reviewsList } from '@/modules/home/reviews/mockedData';
+import { getAverageRating } from '@/shared/utils/getAverageRating';
 import Image from 'next/image';
 
 interface OrderProductProps {
@@ -8,21 +15,49 @@ interface OrderProductProps {
 
 export default function OrderProduct({ currentProduct }: OrderProductProps) {
   const { title, description, available } = currentProduct;
+  const rating = getAverageRating(reviewsList);
 
   return (
     <div className="mb-20">
-      <h1 className="mb-4 text-[16px] font-semibold leading-[120%] uppercase">
-        {title}
-      </h1>
+      <div className="flex items-center justify-between mb-4">
+        <h1 className="text-[16px] font-semibold leading-[120%] uppercase">
+          {title}
+        </h1>
+        <IconButton>
+          <Image
+            src="/images/productPage/orderProduct/share.svg"
+            alt="share icon"
+            width={20}
+            height={20}
+          />
+        </IconButton>
+      </div>
       <p className="mb-6 text-[14px] font-light leading-[120%] text-justify">
         {description}
       </p>
-      <p
-        className={`text-[12px] font-normal leading-[120%] ${
-          available ? 'text-green' : 'text-red-500'
-        }`}
-      >
-        {available ? 'В наявності' : 'Немає в наявності'}
+      <div className="flex justify-between items-center">
+        <p
+          className={`text-[12px] font-normal leading-[120%] ${
+            available ? 'text-green' : 'text-red-500'
+          }`}
+        >
+          {available ? 'В наявності' : 'Немає в наявності'}
+        </p>
+        <Rating
+          initialValue={rating}
+          allowFraction
+          readonly
+          className="mb-1"
+          emptyIcon={
+            <StarEmptyIcon className="inline-block mx-0.5 size-4 xl:size-5" />
+          }
+          fillIcon={
+            <StarFilledIcon className="inline-block mx-0.5 size-4 xl:size-5" />
+          }
+        />
+      </div>
+      <p className="mb-6 xl:mb-[26.5px] text-right text-[10px] xl:text-[14px] font-light leading-[120%]">
+        ({reviewsList?.length} відгуків)
       </p>
       <div className="flex items-center gap-x-4">
         <MainButton
