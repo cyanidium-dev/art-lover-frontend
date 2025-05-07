c7b00f99e6869c8840af07b11e52c37e7ef8b039
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-07 15:21:15 +0300

    added addedToCart popup

--- src/shared/components/productCard/CartButton.tsx
+++ src/shared/components/productCard/CartButton.tsx
@@ -1,19 +1,38 @@
+'use client';
+import { useState } from 'react';
+import Backdrop from '../backdrop/Backdrop';
 import CartIcon from '../icons/CartIcon';
+import AddedToCartPopUp from '../pop-ups/AddedToCartPopUp';
 
 interface CartButtonProps {
   className?: string;
-  onClick?: () => void;
 }
 
-export default function CartButton({ className, onClick }: CartButtonProps) {
+export default function CartButton({ className }: CartButtonProps) {
+  const [isPopUpShown, setIsPopUpShown] = useState(false);
+
+  const handleClick = () => {
+    setIsPopUpShown(true);
+  };
+
   return (
-    <button
-      id="cart-button"
-      onClick={onClick}
-      className={`cursor-pointer relative flex items-center justify-center size-9 xl:w-[46px] xl:h-10 rounded-[8px] text-white bg-orange
+    <>
+      <button
+        id="cart-button"
+        onClick={handleClick}
+        className={`cursor-pointer relative flex items-center justify-center size-9 xl:w-[46px] xl:h-10 rounded-[8px] text-white bg-orange
         xl:hover:brightness-110 focus-visible:brightness-110 active:scale-95 transition duration-300 ease-in-out ${className}`}
-    >
-      <CartIcon />
-    </button>
+      >
+        <CartIcon />
+      </button>
+      <AddedToCartPopUp
+        isPopUpShown={isPopUpShown}
+        setIsPopUpShown={setIsPopUpShown}
+      />
+      <Backdrop
+        isVisible={isPopUpShown}
+        onClick={() => setIsPopUpShown(false)}
+      />
+    </>
   );
 }
