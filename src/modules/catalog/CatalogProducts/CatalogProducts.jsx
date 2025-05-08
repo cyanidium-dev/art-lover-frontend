4c76bb865b83270df753dbb0a189c92a32477fae
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2025-05-08 14:58:08 +0200

    fix

--- src/modules/catalog/CatalogProducts/CatalogProducts.jsx
+++ src/modules/catalog/CatalogProducts/CatalogProducts.jsx
@@ -2,9 +2,9 @@
 import { useState } from 'react';
 
 import ProductCard from '../../../shared/components/productCard/ProductCard';
-import AddedToCartPopUp from '@/shared/components/pop-ups/AddedToCartPopUp';
-import CartModal from '@/shared/components/cart/Cart';
-import Backdrop from '@/shared/components/backdrop/Backdrop';
+// import AddedToCartPopUp from '@/shared/components/pop-ups/AddedToCartPopUp';
+// import CartModal from '@/shared/components/cart/Cart';
+// import Backdrop from '@/shared/components/backdrop/Backdrop';
 
 import { productsList } from './items';
 
@@ -21,7 +21,7 @@ const CatalogProducts = () => {
   ));
 
   return (
-    <div className="grid grid-cols-3 gap-3 mb-[140px] mt-[40px]">
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-3 mb-[140px] mt-[33px] md:mt-[40px]">
       {elements}
     </div>
   )
