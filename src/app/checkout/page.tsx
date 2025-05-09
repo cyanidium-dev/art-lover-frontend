f431afb91c316c5ccd4ffc1a271a5bdb6ed9ede4
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-09 14:38:58 +0300

    added suspense

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from 'react';
+import Loader from '@/shared/components/loader/Loader';
 import Checkout from '@/modules/checkout/Checkout';
 
 export default function CheckoutPage() {
   return (
     <>
-      <Checkout />
+      <Suspense fallback={<Loader />}>
+        <Checkout />
+      </Suspense>
     </>
   );
 }
