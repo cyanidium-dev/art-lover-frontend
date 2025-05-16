ffd1a71501bd17c072899d7329cef4d598b4ca95
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-16 09:38:32 +0300

    updated navbar favorite color

--- src/modules/navbar/NavbarFavorite/NavbarFavorite.tsx
+++ src/modules/navbar/NavbarFavorite/NavbarFavorite.tsx
@@ -1,11 +1,20 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import HeartIcon from '@/shared/components/icons/HeartIcon';
 import Link from 'next/link';
 
 const NavbarFavorite = () => {
+  const pathname = usePathname();
+  const isActive = pathname === '/favorites';
+
   return (
     <Link
       href="/favorites"
-      className="cursor-pointer outline-none xl:hover:text-orange focus-visible:text-orange active:scale-95 transition duration-300 ease-in-out"
+      className={`cursor-pointer outline-none active:scale-95 transition duration-300 ease-in-out 
+        xl:hover:text-orange focus-visible:text-orange ${
+          isActive ? 'text-orange' : ''
+        }`}
     >
       <HeartIcon className="size-5 xl:size-[34px]" />
     </Link>
