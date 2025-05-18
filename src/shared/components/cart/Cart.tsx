a9a492b871b143e6abbe0b9389f93b63f50c0895
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-18 09:32:09 +0300

    added translations for burger menu, cart modal

--- src/shared/components/cart/Cart.tsx
+++ src/shared/components/cart/Cart.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import CartList from './CartList';
 import CartTotal from './CartTotal';
 import { AnimatePresence, motion } from 'framer-motion';
+import { useTranslations } from 'next-intl';
 import { useCartStore } from '@/shared/store/cartStore';
 import {
   fadeInAnimation,
@@ -21,6 +22,7 @@ export default function CartModal({
   isPopUpShown,
   setIsPopUpShown,
 }: CartModalProps) {
+  const t = useTranslations('cart');
   const { cartItems } = useCartStore();
 
   return (
@@ -62,7 +64,7 @@ export default function CartModal({
                 <div className="flex items-center gap-x-5">
                   <CartIcon strokeWidth="2" className="size-6 xl:size-[34px]" />
                   <h3 className="text-[20px] xl:text-[24px] font-bold leading-[120%] uppercase text-white">
-                    Кошик
+                    {t('title')}
                   </h3>
                 </div>
                 <button
