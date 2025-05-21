b6379bd913468b4ccf5cd8dd1175eb0731cf591f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-21 13:01:54 +0300

    updated blog page animation

--- src/modules/blog/blogList/BlogList.tsx
+++ src/modules/blog/blogList/BlogList.tsx
@@ -34,7 +34,7 @@ export default function BlogList({ articlesList }: BlogPageProps) {
               initial="hidden"
               whileInView="visible"
               exit="exit"
-              viewport={{ once: true, amount: 0.4 }}
+              viewport={{ once: true, amount: 0.2 }}
               variants={listVariants({
                 staggerChildren: 0.4,
                 delayChildren: 0.8,
