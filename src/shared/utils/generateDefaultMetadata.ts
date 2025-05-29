11106a5323c69c75d6565d43b90e83f4e5b96dff
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-29 18:20:20 +0300

    added opengraph image

--- src/shared/utils/generateDefaultMetadata.ts
+++ src/shared/utils/generateDefaultMetadata.ts
@@ -10,6 +10,14 @@ export function getDefaultMetadata(
     openGraph: {
       title: t('title'),
       description: t('description'),
+      images: [
+        {
+          url: '/src/app/opengraph-image.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'Art Lover',
+        },
+      ],
       type: 'website',
       locale: locale === 'uk' ? 'uk_UA' : locale === 'ru' ? 'ru_RU' : 'en_US',
       siteName: 'Art Lover',
